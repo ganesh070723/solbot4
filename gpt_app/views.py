be27733e11dd1e90fd1ccec8b085f0ddbbf5abf3
ganesh070723 <bbabu8342@gmail.com>
2023-01-18 23:55:42 +0530

    first

---
+++ gpt_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+import openai
+import speech_recognition as sr
+openai.api_key = "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"
+# Create your views here.
+def index(request):
+    arr=[]
+    if request.method == 'POST':
+        if request.POST.get('search') != '':
+          name = request.POST.get('search')
+          def generate_response(prompt):
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=2048,
+                n=1,
+                stop=None,
+                temperature=0.5
+            )
+            return response.choices[0].text
+          
+        user_input = name
+
+        prompt = f"User: {user_input} Bot: "
+        bot_response = generate_response(prompt)
+
+
+       
+        arr.append(bot_response)
+        print("GPT: " + bot_response)
+        context ={
+            'results': arr,
+            'question': name,
+        }
+    if len(arr)>0:
+        return render(request,'index.html',context)
+    else:
+       return render(request,'index.html')
